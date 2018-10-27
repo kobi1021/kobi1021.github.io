a3c253ba34bb5808ab0b77e0fd5f357488b2e96f
Biko Allen <biko.allen@gmail.com>
2018-10-26 23:12:19 -0500

    Updates

--- precache-manifest.3f0274b747db3eb22fb3f613c00dee73.js
+++ precache-manifest.3f0274b747db3eb22fb3f613c00dee73.js
@@ -1,35 +1,35 @@
 self.__precacheManifest = [
   {
-    "revision": "4d94093ba5ac2ccaee21",
-    "url": "/static/css/main.a0263be1.chunk.css"
+    "revision": "da408238128b876cbda6424391f1566f",
+    "url": "/static/media/fa-brands-400.da408238.woff"
   },
   {
-    "revision": "4d94093ba5ac2ccaee21",
-    "url": "/static/js/main.4d94093b.chunk.js"
+    "revision": "e40d422cb995c26c925d",
+    "url": "/static/css/main.36998ab0.chunk.css"
   },
   {
-    "revision": "9f7643dd5bbd32ec6748",
-    "url": "/static/js/1.9f7643dd.chunk.js"
+    "revision": "10fc2bed2ac4323b04a0",
+    "url": "/static/js/1.10fc2bed.chunk.js"
   },
   {
     "revision": "229c360febb4351a89df",
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "b75b4bfe0d58faeced5006c785eaae23",
-    "url": "/static/media/fa-solid-900.b75b4bfe.woff2"
-  },
-  {
     "revision": "6814d0e8136d34e313623eb7129d538e",
     "url": "/static/media/fa-brands-400.6814d0e8.woff2"
+  },
+  {
+    "revision": "b75b4bfe0d58faeced5006c785eaae23",
+    "url": "/static/media/fa-solid-900.b75b4bfe.woff2"
   },
   {
     "revision": "2d0415fa29ea596b7a02c78eddeede20",
     "url": "/static/media/fa-solid-900.2d0415fa.woff"
   },
   {
-    "revision": "da408238128b876cbda6424391f1566f",
-    "url": "/static/media/fa-brands-400.da408238.woff"
+    "revision": "e40d422cb995c26c925d",
+    "url": "/static/js/main.e40d422c.chunk.js"
   },
   {
     "revision": "e8019d507e8cb51d169ab4f94a0cda12",
@@ -56,7 +56,7 @@ self.__precacheManifest = [
     "url": "/static/media/fa-brands-400.83e6c29f.svg"
   },
   {
-    "revision": "eb7eb2020e42a4c3585cc7ca8c975cad",
+    "revision": "7a0fb81f6c4347c5b76c535c7199eb17",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bd12cf3b76c1140382ff0857ea2eaa42.js"
+  "/precache-manifest.3f0274b747db3eb22fb3f613c00dee73.js"
 );
 
 workbox.clientsClaim();
