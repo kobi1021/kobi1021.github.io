5f8672ebe194df44b0daa710802577f00c5d2f04
kobi1021 <[email protected]>
2018-10-30 03:59:41 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
   "/precache-manifest.14649bfbb78e30d6e2cd7ebc4368f726.js"
