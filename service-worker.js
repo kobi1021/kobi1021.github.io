f7e89984c4ed96379d0f68a7c8e3e5cefe23538c
kobi1021 <[email protected]>
2019-02-11 02:08:40 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.95c6d45df87558ac6d0fdaf073bb7756.js"
+  "/precache-manifest.47dcc80eb555f2ee25b1c128c1263f18.js"
 );
 
 workbox.clientsClaim();
