787e51419a452781394342958298334f54e112d7
kobi1021 <[email protected]>
2019-02-15 12:53:13 +0000

    Updates

--- precache-manifest.47dcc80eb555f2ee25b1c128c1263f18.js
+++ precache-manifest.47dcc80eb555f2ee25b1c128c1263f18.js
@@ -8,8 +8,8 @@ self.__precacheManifest = [
     "url": "/static/js/main.cad36b24.chunk.js"
   },
   {
-    "revision": "42dcff549141ed545983",
-    "url": "/static/js/1.42dcff54.chunk.js"
+    "revision": "39e212935edd92309587",
+    "url": "/static/js/1.39e21293.chunk.js"
   },
   {
     "revision": "229c360febb4351a89df",
@@ -56,7 +56,7 @@ self.__precacheManifest = [
     "url": "/static/media/fa-brands-400.83e6c29f.svg"
   },
   {
-    "revision": "248406da1280bae05f2d883e19bdab16",
+    "revision": "b803d917e4dc5c4688b88ad7a36f6e33",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e759782310386d0e8c1c31635720485c.js"
+  "/precache-manifest.47dcc80eb555f2ee25b1c128c1263f18.js"
 );
 
 workbox.clientsClaim();
